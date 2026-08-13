71e73e59cc78ccb6bf87b3150ce0c42905420043
suvrotica <suvrotica@gmail.com>
2026-08-13 21:01:11 +0530

    Add Holy Allergy post

--- src/lib/generated/media-gallery.ts
+++ src/lib/generated/media-gallery.ts
@@ -3429,6 +3429,13 @@ export const mediaGallery: Record<MediaGalleryTab, MediaGalleryAsset[]> = {
 			"height": 630
 		},
 		{
+			"name": "holy-allergy.jpg",
+			"src": "/images/holy-allergy.jpg",
+			"bytes": 414422,
+			"width": 1012,
+			"height": 1440
+		},
+		{
 			"name": "IMG-20260422-WA0001.jpg",
 			"src": "/images/IMG-20260422-WA0001.jpg",
 			"bytes": 409248,
