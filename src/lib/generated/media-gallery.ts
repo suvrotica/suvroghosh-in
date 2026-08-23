5cb590193588a30236c82d218ce9e82e1d37a1f3
suvrotica <suvrotica@gmail.com>
2026-08-23 15:15:58 +0530

    Publish Morbid Napping

--- src/lib/generated/media-gallery.ts
+++ src/lib/generated/media-gallery.ts
@@ -3947,6 +3947,13 @@ export const mediaGallery: Record<MediaGalleryTab, MediaGalleryAsset[]> = {
 			"height": 630
 		},
 		{
+			"name": "morbid-napping.jpg",
+			"src": "/images/morbid-napping.jpg",
+			"bytes": 367684,
+			"width": 1050,
+			"height": 1498
+		},
+		{
 			"name": "mysteries-of-mental-illness-pbs-series.jpg",
 			"src": "/images/mysteries-of-mental-illness-pbs-series.jpg",
 			"bytes": 101337,
